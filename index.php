f26155cf2a1aa925fe2b5f31037bdb3e925072e1
baggette <58569691+Baggette@users.noreply.github.com>
2024-04-01 22:32:06 -0500

    stuff

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ob_start();
 $text = " ";
 visitor($text);
 function visitorlogger($ip) {
-    $file = fopen('../evisitors.txt', 'a') or die('unable to open file');
+    $file = fopen('../../evisitors.txt', 'a') or die('unable to open file');
     fwrite($file, $ip);
     fclose($file);
     $counter = fopen("counter.txt", "r+") or die("<script> console.log('cannot access counter file') </script>");
@@ -19,7 +19,7 @@ function visitor(&$visitor){
     $visitor = $_SERVER["HTTP_CF_CONNECTING_IP"].PHP_EOL;
   }
 }
-if(strpos(file_get_contents('../evisitors.txt'), $text) !== false) {
+if(strpos(file_get_contents('../../evisitors.txt'), $text) !== false) {
 
 } else {
   visitorlogger($text);
@@ -35,20 +35,28 @@ if(strpos(file_get_contents('../evisitors.txt'), $text) !== false) {
   <meta name="og:description" content="ESTROGEN HIEST">
   <link rel="stylesheet" href="style.css">
   <script type="text/javascript" src="./js/script.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dheereshagrwal/colored-icons@1.7.5/src/app/ci.min.css"/>
 </head>
 <body>
+  <div class="header">
+  <div class="title">
   <h1>ESTROGEN HIEST</h1>
+  </div>
+  <div class="socials">
+    <ul id="socials">
+    <li>
+    <a href="https://open.spotify.com/artist/5n2BT6qtJoparlqZtjcmO0"><i class="ci ci-spotify ci-2x"></i><a>&nbsp;
+    <a href="https://www.instagram.com/spectrums.band/"><i class="ci ci-instagram ci-2x"></i><a>&nbsp;
+    <a href="https://www.youtube.com/@spectrums."><i class="ci ci-youtube ci-2x"></i><a>&nbsp;
+    <a href="https://twitter.com/spectrums_band"><i class="ci ci-twitter ci-2x"></i><a>&nbsp;
+    <a href="https://www.tiktok.com/@spectrums.band"><i class="ci ci-tiktok ci-2x"></i><a>&nbsp;
+</li>
+</ul>
+</div>
+</div>
   <img src="./assets/estrogen_hiest.png" alt="estrogen_hiest">
-  <h2>Releases April 5th 2024</h2>
-<div class="container">
-<div class="iframe">
-  <!--<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/5n2BT6qtJoparlqZtjcmO0?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>-->
-  <p>presave album thingy will be here <br> and maybe other links to other platforms</p>
-</div>
-<div class="insta_widget">
-  <p>this will be insta widget</p>
-</div>
-</div>
+  <h2 id="release"></h2>
+  <p>In the meantime go follow the band on their numerous socials</p>
 <div class="visitors">
     <p id="visitors"></p>
   </div>
--- js/script.js
+++ js/script.js
@@ -25,4 +25,33 @@ function visitors() {
 	const url = './counter.txt'
 	fetch(url).then(r => r.text()).then(t => {const count = document.getElementById('visitors').innerHTML = 'Visitors: ' + t});
 }
+
+function Timetil(){
+	var countDownDate = new Date("April 5, 2024 00:00:00").getTime();
+	var x = setInterval(function() {
+
+		// Get today's date and time
+		var now = new Date().getTime();
+	  
+		// Find the distance between now and the count down date
+		var distance = countDownDate - now;
+	  
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	  
+		// Display the result in the element with id="demo"
+		document.getElementById("release").innerHTML = "The Release is in " + days + " days " + hours + " hours "
+		+ minutes + " minutes " + seconds + " seconds ";
+	  
+		// If the count down is finished, write some text
+		if (distance < 0) {
+		  clearInterval(x);
+		  document.getElementById("release").innerHTML = "<a href='https://open.spotify.com/artist/5n2BT6qtJoparlqZtjcmO0'>OUT NOW ON SPOTIFY</a>";
+		}
+	  }, 1000);
+}
+Timetil()
 visitors()
